1e4cb45fc91dd0da37dcc734bb43065c304ae9a0
Roselia1 <122234195+Roselia1@users.noreply.github.com>
2023-04-01 15:49:15 +0200

    Ajout exo 3

--- TP2Java/src/HelloUniverse.java
+++ TP2Java/src/HelloUniverse.java
@@ -12,13 +12,31 @@ public class HelloUniverse {
            // System.out.println("En " +numéroannée + ",les planètes du systèmes solaire étaient au nombre de:" +nombredeplanetes );
        // }
         //Exo 2
+       // int nombredeplanetes = 8;
+        //int numéroannée = 2006;
+       // if (numéroannée < 2006) {
+          //  nombredeplanetes = 9;
+          //  System.out.println("En " +numéroannée + ",les planètes du systèmes solaire étaient au nombre de:" +nombredeplanetes );
+      //  }
+       // else nombredeplanetes = 8;
+       // System.out.println("En " +numéroannée + ",les planètes du systèmes solaire étaient au nombre de:" +nombredeplanetes );
+       // Exo 3
         int nombredeplanetes = 8;
-        int numéroannée = 2006;
-        if (numéroannée < 2006) {
-            nombredeplanetes = 9;
-            System.out.println("En " +numéroannée + ",les planètes du systèmes solaire étaient au nombre de:" +nombredeplanetes );
+        int numéroannée = 1650;
+        String phrase = "En %d, le nombre de planètes est de : %d.\n"; //formatage de chaines de caractères
+
+        if (numéroannée < 1600 || numéroannée > 2020) {
+            System.out.println("Le programme ne peut pas fournir de résultat pour l'année " +numéroannée);
+        } else
+            if (numéroannée < 1700){
+                nombredeplanetes = 7;
+                System.out.printf(phrase,numéroannée,nombredeplanetes);
+            } else if (numéroannée < 1800) {
+                nombredeplanetes = 8;
+                System.out.println("En " +numéroannée+ " le nombre de planètes est de " +nombredeplanetes);
+            }
+
+
         }
-        else nombredeplanetes = 8;
-        System.out.println("En " +numéroannée + ",les planètes du systèmes solaire étaient au nombre de:" +nombredeplanetes );
     }
-}
+
