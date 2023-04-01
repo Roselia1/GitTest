609219675d1b5c772df4465d84aaab17a12a83e3
Roselia1 <122234195+Roselia1@users.noreply.github.com>
2023-04-01 09:14:15 +0200

    Ajout Exo 2 TPAJava

--- TP2Java/src/HelloUniverse.java
+++ TP2Java/src/HelloUniverse.java
@@ -1,15 +1,23 @@
 public class HelloUniverse {
     public static void main(String[] args) {
+        //int nombredeplanetes = 8;
+        //int numéroannée = 2003;
+        //if (numéroannée < 2006){
+         //   nombredeplanetes = 9;
+         //   System.out.println("En " +numéroannée + ",les planètes du systèmes solaire étaient au nombre de:" +nombredeplanetes );
+       // }
+
+       // if ( numéroannée>= 2006 ){
+          //  nombredeplanetes = 8;
+           // System.out.println("En " +numéroannée + ",les planètes du systèmes solaire étaient au nombre de:" +nombredeplanetes );
+       // }
         int nombredeplanetes = 8;
-        int numéroannée = 2003;
-        if (numéroannée < 2006){
+        int numéroannée = 2006;
+        if (numéroannée < 2006) {
             nombredeplanetes = 9;
             System.out.println("En " +numéroannée + ",les planètes du systèmes solaire étaient au nombre de:" +nombredeplanetes );
         }
-
-        if ( numéroannée>= 2006 ){
-            nombredeplanetes = 8;
-            System.out.println("En " +numéroannée + ",les planètes du systèmes solaire étaient au nombre de:" +nombredeplanetes );
-        }
+        else nombredeplanetes = 8;
+        System.out.println("En " +numéroannée + ",les planètes du systèmes solaire étaient au nombre de:" +nombredeplanetes );
     }
 }
